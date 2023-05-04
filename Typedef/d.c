0cdb28b9013fe1181590a02162723f5745ba8c88
mironprog <bmaresz90@gmail.com>
2023-05-04 17:49:54 +0200

    Short introduction to typedef

---
+++ Typedef/d.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef int kilometer;
+
+int main(void)
+{
+
+    kilometer newyork, chicago, miami;
+
+    newyork = 500;
+    chicago = 300;
+    miami = 1900;
+
+    printf("New York: %d\n", newyork);
+    printf("Chicago: %d\n", chicago);
+    printf("Miami: %d\n", miami); 
+
+    return 0;
+}
